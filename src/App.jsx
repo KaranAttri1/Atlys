414e0b91afb9a29c5bde635735d1bf9bc601c735
Karan Attri <karanattri@Karans-MacBook-Pro.local>
2024-08-20 14:37:22 +0530

    code cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-// import { useState } from 'react'
-
 import { Routes, Route } from 'react-router-dom'
 import ModalPopUp from './components/modal'
 import Feed from './components/Feed'
 import SignUp from './components/SignUp'
 
 function App() {
-  // const[count, setCount] = useState(0)
-  // const[todos, setTodo]  = useState([{title: "Gym", description:"7-9pm"},{title:"study",description:"Habitstrong"}])
   return (
     <>
       <Routes>
@@ -19,15 +11,6 @@ function App() {
         <Route path="/login" element={<ModalPopUp />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
-    {/* <CustomButton count={count} setCount={setCount}></CustomButton>
-    <div>****************************</div>
-    {
-      todos.map((todo)=>{
-        return (<Todo todo={todo}></Todo>)
-      })
-    } */}
-    {/* <div>****************************</div> */}
-    {/* <ModalPopUp></ModalPopUp> */}
     </>
   )
 }
